273e02aa87a0fe5c8f1b4f249fc203754061cbe9
灿烂 <1481086138@qq.com>
2024-05-04 01:59:52 +0800

    处理编辑功能

--- src/App.vue
+++ src/App.vue
@@ -1,64 +1,62 @@
 <template>
   <div id="app">
-  <section class="todoapp">
-    <header class="header">
-      <h1>todos</h1>
-      <input
-        class="new-todo"
-        autofocus=""
-        autocomplete="off"
-        placeholder="What needs to be done?"
-        v-model="newTodoItem"
-        @keyup.enter="addTodoItem"
-      />
-    </header>
-    <section class="main">
-      <input id="toggle-all" class="toggle-all" type="checkbox" />
-      <label for="toggle-all">Mark all as complete</label>
-      <ul class="todo-list">
-        <li class="todo" :class="{completed: item.completed}" v-for="item in list" :key="item.id">
-          <div class="view">
-            <input class="toggle" type="checkbox" v-model="item.completed" />
-            <label>{{ item.title }}</label>
-            <button class="destroy"></button>
-          </div>
-          <input class="edit" type="text" />
-        </li>
-      </ul>
+    <section class="todoapp">
+      <header class="header">
+        <h1>todos</h1>
+        <input class="new-todo" autofocus="" autocomplete="off" placeholder="What needs to be done?"
+          v-model="newTodoItem" @keyup.enter="addTodoItem" />
+      </header>
+      <section class="main">
+        <input id="toggle-all" class="toggle-all" type="checkbox" :checked="allCompleted" @input="changeAllChecked($event.target.checked)" />
+        <label for="toggle-all">Mark all as complete</label>
+        <ul class="todo-list">
+          <li class="todo" :class="{ completed: item.completed, editing: item === editItem }"
+            @dblclick="editTodoItem(item)" v-for="item in list" :key="item.id">
+            <div class="view">
+              <input class="toggle" type="checkbox" v-model="item.completed" />
+              <label>{{ item.title }}</label>
+              <button class="destroy"></button>
+            </div>
+            <input class="edit" type="text" v-model="item.title" @keyup.enter="doneEdit(item.title)"
+              @blur="doneEdit(item.title)" />
+          </li>
+        </ul>
+      </section>
+      <footer class="footer">
+        <span class="todo-count">
+          <strong>{{ activelist.length }}</strong>
+          <span>item{{ activelist.length > 1 ? 's' : '' }} left</span>
+        </span>
+        <ul class="filters">
+          <li><a href="#/all" :class="{ selected: status === 'all' }">All</a></li>
+          <li><a href="#/active" :class="{ selected: status === 'active' }">Active</a></li>
+          <li><a href="#/completed" :class="{ selected: status === 'completed' }">Completed</a></li>
+        </ul>
+        <button class="clear-completed" v-show="completedlist.length > 0">
+          Clear completed
+        </button>
+      </footer>
     </section>
-    <footer class="footer">
-      <span class="todo-count">
-        <strong>{{ activelist.length }}</strong>
-        <span>item{{activelist.length > 1 ? 's' : ''}} left</span>
-      </span>
-      <ul class="filters">
-        <li><a href="#/all" :class="{selected: status === 'all' }">All</a></li>
-        <li><a href="#/active" :class="{selected: status === 'active' }">Active</a></li>
-        <li><a href="#/completed" :class="{selected: status === 'completed' }">Completed</a></li>
-      </ul>
-      <button class="clear-completed" v-show="completedlist.length > 0">
-        Clear completed
-      </button>
-    </footer>
-  </section>
-</div>
+  </div>
 
 </template>
 <script>
-  import useTodoList from './composition/useTodoList'
-  import useAddTodoItem from './composition/useAddTodoItem'
-  import useFilter from './composition/useFilter'
+import useTodoList from './composition/useTodoList'
+import useAddTodoItem from './composition/useAddTodoItem'
+import useFilter from './composition/useFilter'
+import useEditItem from './composition/useEditItem'
 
-  export default {
-    name: 'App',
-    setup() {
-      const { todos } = useTodoList()
-      
-      
-      return { 
-        ...useAddTodoItem(todos),// 展开该函数返回的每一项
-        ...useFilter(todos)
-      }
+export default {
+  name: 'App',
+  setup() {
+    const { todos } = useTodoList()
+
+
+    return {
+      ...useAddTodoItem(todos),// 展开该函数返回的每一项
+      ...useFilter(todos),
+      ...useEditItem(todos),
     }
   }
+}
 </script>
---
+++ src/composition/useEditItem.js
@@ -0,0 +1,42 @@
+import { computed, ref } from "vue";
+
+export default function useEditItem(todos) {
+  const editItem = ref(null)
+  const isAllCompleted = ref(true)
+  
+  // 获得要编辑的item
+  const editTodoItem = (item) => {
+    editItem.value = item
+  }
+
+  // 完成编辑
+  const doneEdit = (title) => {
+    // 判断 title 是否是空值
+    if (title.trim() === '') { // 如果是空值删除当前项
+      todos.value.splice(todos.value.indexOf(editItem.value), 1)
+    } else {
+      editItem.value = title.trim()
+    }
+    editItem.value = null
+  }
+
+  // 处理全选
+  const allCompleted = computed(() => {
+    return todos.value.filter(item => !item.completed).length === 0
+  })
+
+  const changeAllChecked = (checked) => {
+    todos.value.forEach(item => {
+      item.completed = checked
+    })
+  }
+
+
+  return {
+    editItem,
+    editTodoItem,
+    doneEdit,
+    allCompleted,
+    changeAllChecked,
+  }
+}
